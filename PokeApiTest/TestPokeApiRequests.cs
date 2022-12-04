b9d274b4583be2608ccc599f4d6e794d5805428c
Sarpps <msarpongboateng@gmail.com>
2022-12-04 20:02:15 +0000

    New test for Translated Poke API data

--- PokeApiTest/TestPokeApiRequests.cs
+++ PokeApiTest/TestPokeApiRequests.cs
@@ -26,4 +26,24 @@ public class TestPokeApiRequests
 
         Assert.AreEqual(expectedResult, resultValue);
     }
+
+    public void TestTranslatedPokemonInfoSuccess()
+    {
+        var expectedResult = new PokedexResponse
+        {
+            Name = "clefairy",
+            Description = "Many admirers,  its magical and cute appeal has.Rare and found only in certain areas,  it is.",
+            Habitat = "mountain",
+            IsLegendary = false
+        };
+
+        _pokedexService.Setup(p => p.ReturnBasicPokemonInfo("clefairy")).Returns(Task.FromResult(expectedResult));
+
+        var pokedexController = new PokedexController(_logger.Object, _pokedexService.Object);
+        
+        var result = pokedexController.GetTranslatedPokemonInformation("clefairy");
+        var resultValue = result.Result.Value;
+
+        Assert.AreEqual(expectedResult, resultValue);
+    }
 }
